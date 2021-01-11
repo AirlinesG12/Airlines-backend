20cb284e68af10c9294c5bcf5e9d5a8719ec29b1
sivarajgs <sivarajfairlands@gmail.com>
2021-01-11 12:40:28 +0530

    Booking Resources

---
+++ Airlines/src/main/java/com/lti/repository/BookingRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.lti.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lti.entity.Bookings;
+import com.lti.entity.Flight;
+
+@Repository
+public class BookingRepositoryImpl implements BookingRepository {
+	
+	@PersistenceContext
+	EntityManager em;
+	
+	@Transactional
+	public long bookATicket(Bookings booking) {
+		return em.merge(booking).getBookingId();
+	}
+	
+	public Bookings findBookingsByBookingId(long bookingId) {
+		return em.find(Bookings.class, bookingId);
+	}
+	public Flight findFlightById(long flightId) {
+		return em.find(Flight.class, flightId);
+	}
+	
+	public Bookings findBookingsByUserId(long userId){
+		return em.find(Bookings.class, userId);
+	}
+	
+}
